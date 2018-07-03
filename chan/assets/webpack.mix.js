a44dec0cc781de9539edbcb381e1c3ca031759a8
kamidev <jonas.beckman@gmail.com>
2018-07-03 10:21:32 +0200

    Switch from Brunch to Webpack

---
+++ chan/assets/webpack.mix.js
@@ -0,0 +1,9 @@
+mix.setPublicPath('../priv/static')
+  .js('js/app.js', 'js/app.js')
+  .sass('css/app.scss', 'css/app.css')
+  .copyDirectory('./static', '../priv/static');
+
+mix.options({
+  clearConsole: false,
+  processCssUrls: false
+});
